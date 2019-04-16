71af2680d31057b804afbc2febced6f7b198e7a1
Jannis Schaefer <j.schaefer@estwx.de>
2019-04-16 15:00:32 +0200

    initial commit

---
+++ client/client.go
@@ -0,0 +1,69 @@
+package client
+
+import (
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type Client interface {
+	GetDesiredTemperature(ain string) (float32, error)
+	GetCurrentTemperature(ain string) (float32, error)
+	GetComfortTemperature(ain string) (float32, error)
+	GetSavingTemperature(ain string) (float32, error)
+}
+
+type client struct {
+	http    http.Client
+	rootUrl url.URL
+}
+
+func New() (*client, error) {
+	return nil, nil
+}
+
+func (c *client) GetDesiredTemperature(ain string) (float32, error) {
+	return 0, nil
+}
+
+func (c *client) GetCurrentTemperature(ain string) (float32, error) {
+	return 0, nil
+}
+
+func (c *client) GetComfortTemperature(ain string) (float32, error) {
+	return 0, nil
+}
+
+func (c *client) GetSavingTemperature(ain string) (float32, error) {
+	return 0, nil
+}
+
+func (c *client) getSid() error {
+	resp, err := http.Get(fmt.Sprintf("%s/login_sid.lua", c.rootUrl.String()))
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	resXml := xml.NewDecoder(resp.Body)
+
+	for {
+		t, err := resXml.Token()
+		if err != nil {
+
+		}
+		if t == nil {
+			break
+		}
+
+		switch se := t.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "sid" {
+
+			}
+		}
+	}
+
+	return nil
+}
